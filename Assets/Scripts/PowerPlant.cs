b4e6d0ec88cf79a6c9abbd2c252a6cee96d7f019
Burak <90563983+burakgulirmak35@users.noreply.github.com>
2023-07-12 20:42:02 +0300

    ++

--- Assets/Scripts/PowerPlant.cs
+++ Assets/Scripts/PowerPlant.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class PowerPlant : MonoBehaviour
 {
     [SerializeField] private SettingsSO settings;
+    [Space]
+    [SerializeField] private Image imgFill;
+    [SerializeField] private TextMeshPro txtIncome;
 
     private void Start()
     {
