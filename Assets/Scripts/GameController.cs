64015b5d1137d22cdd2f16ed637ca0142fa5e7e3
Burak <90563983+burakgulirmak35@users.noreply.github.com>
2023-07-13 20:21:50 +0300

    ++

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,11 +6,13 @@ using UnityEngine.EventSystems;
 public class GameController : MonoBehaviour
 {
     public static GameController Instance { get; private set; }
-    private Vector3 StartPosition;
+    private Vector3 startPosition;
     private List<SoldierUnit> selectedUnitList = new List<SoldierUnit>();
+    [SerializeField] private Transform selectionAreaTransform;
     private void Awake()
     {
         Instance = this;
+        selectionAreaTransform.gameObject.SetActive(false);
     }
 
     private void Update()
@@ -18,31 +20,64 @@ public class GameController : MonoBehaviour
         if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
             BuildingManager.Instance.Build();
-            StartPosition = UtilsClass.GetMouseWorldPosition();
+            startPosition = UtilsClass.GetMouseWorldPosition();
+            selectionAreaTransform.gameObject.SetActive(true);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 currentMousePosition = UtilsClass.GetMouseWorldPosition();
+            Vector3 lowerLeft = new Vector3(
+                Mathf.Min(startPosition.x, currentMousePosition.x),
+                Mathf.Min(startPosition.y, currentMousePosition.y)
+            );
+            Vector3 upperRight = new Vector3(
+                Mathf.Max(startPosition.x, currentMousePosition.x),
+                Mathf.Max(startPosition.y, currentMousePosition.y)
+            );
+            selectionAreaTransform.position = lowerLeft;
+            selectionAreaTransform.localScale = upperRight - lowerLeft;
         }
         if (Input.GetMouseButtonUp(0))
         {
-            Collider2D[] collider2DArray = Physics2D.OverlapAreaAll(StartPosition, UtilsClass.GetMouseWorldPosition());
-            selectedUnitList.Clear();
-            foreach (Collider2D collider2D in collider2DArray)
-            {
-                SoldierUnit soldierUnit = collider2D.GetComponent<SoldierUnit>();
-                if (soldierUnit != null)
-                {
-                    selectedUnitList.Add(soldierUnit);
-                }
-            }
+            SelectUnits();
         }
         if (Input.GetMouseButtonDown(1) && !EventSystem.current.IsPointerOverGameObject())
         {
             BuildingManager.Instance.SetFlag();
+            Vector3 moveToPosition = UtilsClass.GetMouseWorldPosition();
+
+            foreach (SoldierUnit soldierUnit in selectedUnitList)
+            {
+                soldierUnit.MoveTo(moveToPosition);
+            }
+
         }
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             UIManager.Instance.UnSelectBuildingType();
         }
-
         BuildingManager.Instance.ShowGhost();
+    }
+
+    private void SelectUnits()
+    {
+        selectionAreaTransform.gameObject.SetActive(false);
+
+        Collider2D[] collider2DArray = Physics2D.OverlapAreaAll(startPosition, UtilsClass.GetMouseWorldPosition());
+        foreach (SoldierUnit soldierUnit in selectedUnitList)
+        {
+            soldierUnit.SetSelectedVisible(false);
+        }
+        selectedUnitList.Clear();
+        foreach (Collider2D collider2D in collider2DArray)
+        {
+            SoldierUnit soldierUnit = collider2D.GetComponent<SoldierUnit>();
+            if (soldierUnit != null)
+            {
+                soldierUnit.SetSelectedVisible(true);
+                selectedUnitList.Add(soldierUnit);
+            }
+        }
     }
 
 }
--- Assets/Scripts/SoldierUnit.cs
+++ Assets/Scripts/SoldierUnit.cs
@@ -1,17 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SoldierUnit : MonoBehaviour
 {
     [SerializeField] private UnitSO unitSO;
+    [Space]
     private int unitHealth;
     private int unitPower;
+    [Space]
+    private GameObject selectedGameObject;
+    private NavMeshAgent agent;
 
     private void Awake()
     {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
+        selectedGameObject = transform.Find("Selected").gameObject;
         unitHealth = unitSO.unitHealth;
         unitPower = unitSO.unitPower;
+    }
+
+    public void SetSelectedVisible(bool visible)
+    {
+        selectedGameObject.SetActive(visible);
+    }
+
+    public void MoveTo(Vector3 targetPosition)
+    {
+        agent.SetDestination(targetPosition);
     }
 
 }
